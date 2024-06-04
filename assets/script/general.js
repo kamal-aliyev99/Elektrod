ad5398cc37606b4c3de62fad5fdf0ef8ceb6fe97
kamal-aliyev <myangels.n1@gmail.com>
2024-06-04 12:14:39 +0400

    header improved

--- assets/script/general.js
+++ assets/script/general.js
@@ -6,6 +6,15 @@ const headerGroupDropdown = document.querySelector('.header__groups--dropdown');
 headerGroups.addEventListener("click", () => {
     headerGroupDropdown.classList.toggle("active");
 })
+
+const headerLang = document.querySelector(".header__lang");
+const headerLangMenu = document.querySelector(".header__lang--menu");
+
+headerLang.addEventListener("click", () => {
+    headerLangMenu.classList.toggle("active");
+})
+
 document.addEventListener("click", (e) => {
     !headerGroups.contains(e.target) && headerGroupDropdown.classList.remove("active");
+    !headerLang.contains(e.target) && headerLangMenu.classList.remove("active");;
 })
