9987a7d4a4f21a1cf4bedba273cc3db47e08b22c
kamal-aliyev <myangels.n1@gmail.com>
2024-06-22 18:06:21 +0400

    sliders added at homePage

--- assets/script/slider.js
+++ assets/script/slider.js
@@ -1,18 +1,31 @@
 import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs'
 
-const swiper = new Swiper(".bannerSwiper", {
+const homeBannerSwiper = new Swiper("#homeBanner", {
     slidesPerView: 1,
     spaceBetween: 30,
     loop: true,
     pagination: {
-      el: ".swiper-pagination",
+      el: "#homeBanner .swiper-pagination",
       clickable: true,
     },
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: "#homeBanner .swiper-button-next",
+      prevEl: "#homeBanner .swiper-button-prev",
     },
     autoplay: {
         delay: 3000,
     },
+}); 
+
+const timeLineSwiper = new Swiper("#timeLine", {
+  slidesPerView: 'auto',
+  spaceBetween: 0,
+  loop: true,
+  navigation: {
+    nextEl: ".timeLine__slider .swiper-button-next",
+    prevEl: ".timeLine__slider .swiper-button-prev",
+  },
+  autoplay: {
+      // delay: 3000,
+  },
 }); 
